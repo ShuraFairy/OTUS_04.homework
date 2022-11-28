f06e1c52b4a26572d4a23b69596946228a001ee9
ShuraFairy <ShuraFairy@users.noreply.github.com>
2022-11-28 14:51:21 +0000

    deploy: 2bec28d3b654aee871d7c4bc397afadd203293be

---
+++ search/functions_9.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['setcontroller_0',['setController',['../class_model.html#a7ec907aaf2f5e4fd4428ea6481c43416',1,'Model']]],
+  ['setdocument_1',['setDocument',['../class_model.html#ac8c3367200a34c0e423fd1fb599c0f95',1,'Model']]],
+  ['square_2',['Square',['../class_square.html#a4ea1091ee1af358f23be4142501caa8f',1,'Square']]]
+];
